bbec34bbf44f4204b96bcfbe5a2b404f4e4f1b9a
Mukhiil <bmukhiil@gmail.com>
2020-11-07 14:30:18 -0800

    gets emotions from image

---
+++ server/emotion_and_emoji_getting.py
@@ -0,0 +1,46 @@
+from azure.cognitiveservices.vision.face import FaceClient
+from msrest.authentication import CognitiveServicesCredentials
+import json, os, requests
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+KEY = "9e52a3a05f2843b3a56ef3eec5653086"
+ENDPOINT = "https://fallhackapi.cognitiveservices.azure.com/"
+LOCATION = "westus2"
+
+
+
+
+
+def get_emotions(image):
+    list_of_emotions = []
+    dict_of_emotions = {}
+    face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+    detected_faces = face_client.face.detect_with_url(url=image,return_face_attributes=["emotion"])
+    
+
+    for face in detected_faces:
+        face_emotion = face.face_attributes.emotion
+        
+        dict_of_emotions['anger'] = face_emotion.anger
+        dict_of_emotions['contempt'] = face_emotion.contempt
+        dict_of_emotions['disgust'] = face_emotion.disgust
+        dict_of_emotions['fear'] = face_emotion.fear
+        dict_of_emotions['happiness'] = face_emotion.happiness
+        dict_of_emotions['neutral'] = face_emotion.neutral
+        dict_of_emotions['sadness'] = face_emotion.sadness
+        dict_of_emotions['surprise'] = face_emotion.surprise
+        
+        biggest_emotion = max(dict_of_emotions, key = dict_of_emotions.get)
+        list_of_emotions.append(biggest_emotion)
+
+    print(list_of_emotions)
+
+    
+
+get_emotions("http://www.historyplace.com/kennedy/president-family-portrait-closeup.jpg")
+
+  
+
+
+    
+
+
