02ddc08badbcf794439052fbc892b79d88065cd3
Mukhiil <bmukhiil@gmail.com>
2020-11-07 15:11:29 -0800

    api skeleton

--- server/api.py
+++ server/api.py
@@ -1,12 +1,20 @@
-from flask import Flask, request
+from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/api', methods=['GET'])
-def index():
-    return 'Index'
 
-@app.route('/api/custom', methods=['POST'])
-def custom():
-    print(request.json['newText'])
-    return request.json['newText']
+class ImageProcessing(Resource):
+    def post(self,image_url):
+
+        
+        return {"image url": image_url}
+
+
+
+api.add_resource(ImageProcessing, "/image/<string:image_url>")
+
+
+if __name__ == "__main__":
+    app.run()
