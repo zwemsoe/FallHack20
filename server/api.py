996b5f2ef1b23b5f8e8597e22e2510f1b996fbea
zsoe <zwemsoe@gmail.com>
2020-11-07 23:19:19 -0800

    still fixing

--- server/api.py
+++ server/api.py
@@ -1,8 +1,6 @@
-
 from flask import Flask, flash, request, jsonify
 from PIL import Image
 import base64
-import numpy as np
 from io import BytesIO
 from flask_restful import Resource, Api
 from base64 import b64encode
@@ -13,7 +11,7 @@ api = Api(app)
 
 class ImageProcessing(Resource):
     def post(self):
-        file = request.files['image']
+        file = request.files['file']
         img_strm = Image.open(file.stream)
         img_strm = img_strm.convert('RGB')
         buffered = BytesIO()
@@ -22,22 +20,11 @@ class ImageProcessing(Resource):
         img_str = img_str64.decode('utf-8')
         raw_data = {"image string": img_str}
         json_img_data = dumps(raw_data, indent=2)
-
-
-
-        
-
-
-        print("img_str64")
-        
-        
-        
+        print("img_str64")       
         return json_img_data
 
 
-
-api.add_resource(ImageProcessing, "/image")
-
+api.add_resource(ImageProcessing, "api/image")
 
 if __name__ == '__main__':
     app.run()
--- server/face_detection.py
+++ server/face_detection.py
@@ -50,8 +50,6 @@ def get_emotions(image):
 
 
   
-
-
     
 
 
