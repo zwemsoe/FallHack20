d1860f830fdb288c67c701ec5972432ce6ad503f
Mukhiil <bmukhiil@gmail.com>
2020-11-11 01:13:22 -0800

    cleaned api

--- server/api.py
+++ server/api.py
@@ -9,45 +9,27 @@ from io import BytesIO
 from flask_restful import Resource, Api
 from base64 import b64encode
 from json import dumps
-from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://fallhackdb:fallhackdb@freecluster.yg7sy.mongodb.net/Test?retryWrites=true&w=majority"
-mongo = PyMongo(app)
+
 
 
 @app.route('/api/imageUpload', methods=['POST'])
 def uploadImage():
+    
     img_file = request.files["file"]
     emotions_list = get_emotions(img_file)
     processed_img = getManipulatedImage(emotions_list, img_file, "random")
-    #processed_img = io.TextIOWrapper(processed_img)
+    
     
     buffered = BytesIO()
     processed_img.save(buffered, format="JPEG")
     processed_img_str = base64.b64encode(buffered.getvalue())
-    print(processed_img_str)
     
-    #mongo.save_file(img_file.filename, processed_img_str)
+    
     return processed_img_str
 
-
-@app.route('/image/<name>', methods=['GET'])
-def image(name):
-    return mongo.send_file(name)
-
-
-@app.route('/api/process/<mode>', methods=['POST'])
-def processImage(mode):
-    img_json = request.form['image']
-
-    #img_strm = io.BytesIO(base64.b64decode(img_json))
-    #img = io.BytesIO(img_strm)
-    print(img_json)
-    #emotions_list = ["anger"]
-    #processed_img = getManipulatedImage(emotions_list, img, mode)
-    print("processed_img")
-    return 'ok'
 
 
 if __name__ == '__main__':
